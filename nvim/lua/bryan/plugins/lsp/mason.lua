8a922ed16042b5892f5d10860d77edecfc3a73a6
Bryan Wills <bryanwi09@gmail.com>
2025-07-15 11:04:26 -0400

    feat: restored and included nvim config in dotfiles and not a separate repo

---
+++ nvim/lua/bryan/plugins/lsp/mason.lua
@@ -0,0 +1,55 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    -- HOW TO ADD MORE LSPs:
+    -- Add the server name to the ensure_installed list below.
+    -- Example: "tsserver" for TypeScript, "pyright" for Python, etc.
+    -- Mason will auto-install them and they will be set up in your lspconfig.
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
+    local mason_tool_installer = require("mason-tool-installer")
+
+    -- enable mason and configure icons
+    mason.setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    mason_lspconfig.setup({
+      -- list of servers for mason to install
+      ensure_installed = {
+        -- Add more language servers here as needed
+        --"tsserver", -- TypeScript/JavaScript
+        "html",
+        "cssls",
+        "tailwindcss",
+        "svelte",
+        "lua_ls",
+        "graphql",
+        "emmet_ls",
+        "prismals",
+        "pyright",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "pylint",
+        "eslint_d",
+      },
+    })
+  end,
+}
