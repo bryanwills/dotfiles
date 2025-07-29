68efa5bcc65f2c1af326274ef7a9d91b3b36c58f
Bryan Wills <bryanwi09@gmail.com>
2025-07-28 21:29:29 -0400

    feat: fixed issues from :healthcheck, added nvim-web-devicons plugin, added Obsidian theme and updated spf/config.toml

--- nvim/lua/bryan/plugins/init.lua
+++ nvim/lua/bryan/plugins/init.lua
@@ -2,5 +2,21 @@ return {
   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
   require("bryan.plugins.theme"),
+
+  -- Add nvim-web-devicons
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = false,
+    config = function()
+      require("nvim-web-devicons").setup({
+        color_icons = true,
+        default = true,
+        strict = true,
+      })
+    end,
+  },
 }
---require("bryan.plugins.theme")
+
+--require("bryan.plugins.theme
+
+-- Add nvim-web-devicons
