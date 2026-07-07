bd3ef6359def73a5821166e92d01eb3a7aefdc7e
Bryan Wills <bryanwi09@gmail.com>
2026-07-06 22:18:52 -0400

    feat: adding powershell custom files

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,39 @@
+# Add `. $env:$USERPROFILE\.config\powershell\user_profile.ps1` to local file ../Documents/Powershell/Microsoft.PowerShell_profile.ps1
+
+Import-Module posh-git
+$omp_config = Join-Path $PSScriptRoot ".\bryan.omp.json"
+oh-my-posh init pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Paths
+$env:PATH += ';.\node_modules\.bin'
+$env:PATH += ';C:\Program Files (x86)\Windows Kits\10\bin\10.0.28000.0\x64'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
